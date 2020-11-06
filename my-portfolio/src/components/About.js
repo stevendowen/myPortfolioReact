5c2a821c4a6796b3513010261e8e93df7deab9c2
Steven Owen <owen.steven.d@gmail.com>
2020-11-06 14:26:39 -0700

    added image and style on about component

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import Me from '../images/me.png';
 
 class About extends Component {
     render () {
         return (
-            <div>
-                <h1 className="name">I'm Steven Owen</h1>
-                <img ></img>
+            <div className="aboutBox">
+                <h1 className="name about">I'm Steven Owen</h1>
+                <div className="imgBox">
+                    <img src={Me} />
+                </div>
                 <p> 
                     I am a highly motivated Web Developer pursuing opportunity to further my career.
-                    I am a recent graduate that was asked by start-up tech company to extend internship 
-                    as a result of outstanding performance and problem-solving skills. I am committed 
-                    to maintaining cutting edge technical skills and up-to-date industry knowledge.
+                    I am a recent graduate from Mountainland Tech College that was asked by start-up 
+                    tech company to extend internship as a result of outstanding performance and 
+                    problem-solving skills. I am currently enrolled at Utah Valley University in a 
+                    Associate Degree program and am committed to maintaining cutting edge technical skills 
+                    and up-to-date industry knowledge.
                 </p>
             </div>
         );
